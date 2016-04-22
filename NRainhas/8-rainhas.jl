f8f60c7482bd500cc91d8e57a9e712ef831c4789
TomasSQ <tomasqueiroga@gmail.com>
2016-04-22 19:21:58 -0300

    first commit

--- NRainhas/8-rainhas.jl
+++ NRainhas/8-rainhas.jl
@@ -4,35 +4,35 @@ PROBLEMAN_SIZE = 10
 POPULATION_SIZE = 200
 
 function ehViavel(solucao)
-  return fitness(solucao) == 0
+    return fitness(solucao) == 0
 end
 
 function imprimeLinha(solucao)
-  print("+")
-  for coluna in eachindex(solucao)
-    print("---+")
-  end
-  println("")
+    print("+")
+    for coluna in eachindex(solucao)
+        print("---+")
+    end
+    println("")
 end
 
 function imprimeSolucao(solucao)
-  imprimeLinha(solucao)
+    imprimeLinha(solucao)
 
-  for linha in eachindex(solucao)
-    print("|")
-    for coluna in eachindex(solucao)
-      print(" ")
-      if coluna == solucao[linha]
-        print("x")
-      else
-        print(" ")
-      end
-      print(" |")
+    for linha in eachindex(solucao)
+        print("|")
+        for coluna in eachindex(solucao)
+            print(" ")
+            if coluna == solucao[linha]
+                print("x")
+            else
+                print(" ")
+            end
+            print(" |")
+        end
+        println("")
+        imprimeLinha(solucao)
     end
     println("")
-    imprimeLinha(solucao)
-  end
-  println("")
 end
 
 solucao = solve(POPULATION_SIZE, () -> zeros(Int8, POPULATION_SIZE, PROBLEMAN_SIZE), () -> shuffle(collect(1:PROBLEMAN_SIZE)))
